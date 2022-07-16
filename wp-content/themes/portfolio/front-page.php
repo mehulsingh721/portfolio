b246ab9381db61ffaa4d99f9c7faa70b56319d7b
Mehul Singh Rathore <mehulsingh072001@gmail.com>
2022-07-16 21:03:21 +0530

    fixed image width and height

--- wp-content/themes/portfolio/front-page.php
+++ wp-content/themes/portfolio/front-page.php
@@ -111,25 +111,28 @@
                             <div class="container__content">
                                 <div class="info">
                                     <h3 class="title heading-3" onclick="openProjectModal(<?php echo the_ID() ?>)"><?php the_title() ?></h3>
-                                    <p class="description" onclick="openProjectModal(<?php echo the_ID() ?>)"><?php the_field("intro_description") ?></p>
+                                    <p class="description"><?php the_field("intro_description") ?></p>
                                     <?php
                                     $string = the_field("tech_stack");
                                     echo $string;
                                     ?>
-                                    <ul class="links">
-                                        <li class="link"><a href="<?php the_field("github_link")?>"><svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
-                                                    <title>GitHub</title>
-                                                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-                                                </svg></a></li>
-                                        <li class="link">
-                                            <a href="<?php the_field("project_link") ?>" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link">
-                                                    <title>External Link</title>
-                                                    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-                                                    <polyline points="15 3 21 3 21 9"></polyline>
-                                                    <line x1="10" y1="14" x2="21" y2="3"></line>
-                                                </svg></a>
-                                        </li>
-                                    </ul>
+                                    <div class="actions">
+                                        <ul class="links">
+                                            <li class="link"><a href="<?php the_field("github_link") ?>" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
+                                                        <title>GitHub</title>
+                                                        <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                                                    </svg></a></li>
+                                            <li class="link">
+                                                <a href="<?php the_field("project_link") ?>" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link">
+                                                        <title>External Link</title>
+                                                        <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                                                        <polyline points="15 3 21 3 21 9"></polyline>
+                                                        <line x1="10" y1="14" x2="21" y2="3"></line>
+                                                    </svg></a>
+                                            </li>
+                                        </ul>
+                                        <button class="details" onclick="openProjectModal(<?php echo the_ID() ?>)">Details <span>&rarr;</span></button>
+                                    </div>
                                 </div>
                             </div>
                             <div class="container__image">
